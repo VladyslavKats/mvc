83699e105a1628be57cb3fa41ba99b56e80b00d8
Vladyslav Kats <kac9661@gmail.com>
2023-09-04 15:58:49 +0300

    add action filer

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -3,19 +3,36 @@
 
     class Posts extends \Core\Controller{
 
-        public function index(){
+        public function indexAction(){
             echo htmlspecialchars(print_r($_GET , true));
         }
 
-        public function addNew(){
+        public function addNewAction(){
             echo 'Hello from the addNew action in the post controller';
         }
 
-        public function edit(){
+        public function editAction(){
             echo $this->route_params['id'];
         }
 
-    
+        public function __call($name , $args){
+            $method = $name . 'Action';
+            if(method_exists($this , $method)){
+                if($this->before()){
+                    call_user_func_array([$this , $method],$args);
+                    $this->after();
+                }
+            }
+        }
+
+        protected function before() : bool{
+            echo '(before)';
+            return true;
+        }
+
+        protected function after(){
+            echo '(after)';
+        }
     }
 
 ?>
