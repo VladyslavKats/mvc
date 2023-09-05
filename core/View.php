73c615c5a12e32f3754f8bd7bfc778f0f52734e6
Vladyslav Kats <kac9661@gmail.com>
2023-09-05 11:10:26 +0300

    add simple view

---
+++ core/View.php
@@ -0,0 +1,16 @@
+<?php
+    namespace Core;
+
+    class View{
+        public static function render($view , $args = []){
+            $file = "../App/Views/$view";
+            extract($args ,EXTR_SKIP);
+            if(is_readable($file)){
+                require $file;
+            }else{
+                echo "$file not found";
+            }
+        }
+    }
+
+?>
